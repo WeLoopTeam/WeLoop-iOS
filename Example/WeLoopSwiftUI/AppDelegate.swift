eaf2dc794fb1e23f168ce9ae26667116bb8320b1
HHK1 <henryhuck@hotmail.fr>
2020-12-06 19:39:45 +0100

    modify example to use a SceneDelegate and add an example using SwiftUI

--- Example/WeLoopSwiftUI/AppDelegate.swift
+++ Example/WeLoopSwiftUI/AppDelegate.swift
@@ -1,17 +1,17 @@
 //
 //  AppDelegate.swift
-//  WeLoop
+//  WeLoopSwiftUI
 //
-//  Created by HHK1 on 04/04/2019.
-//  Copyright (c) 2019 HHK1. All rights reserved.
+//  Created by Henry Huck on 06/12/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
 //
 
 import UIKit
 import WeLoop
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     // This is a fake project GUID. Replace it with your actual project ID to test the example
     private let projectGUID = "e19340c0-b453-11e9-8113-1d4bacf0614e"
     private let user = User(id: "1", email: "test1@yopmail.com", firstName: "test1", lastName: "test2")
@@ -22,14 +22,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 
         // Set the invocation preferences. You can always change them after invoking the SDK
         WeLoop.set(invocationMethod: .fab)
-        
+
         WeLoop.set(delegate: self)
         WeLoop.initialize(apiKey: projectGUID);
         WeLoop.authenticateUser(user: user)
-    
         return true
     }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
 }
+
 
 extension AppDelegate: WeLoopDelegate {
     
@@ -51,3 +65,4 @@ extension AppDelegate: WeLoopDelegate {
         print(newCount)
     }
 }
+
---
+++ Example/WeLoopSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeLoopSwiftUI
+//
+//  Created by Henry Huck on 06/12/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
